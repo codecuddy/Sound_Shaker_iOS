fa4102bcb5310980d40cc00633f182a4c48775fd
Charlie <charliecuddy4@gmail.com>
2018-01-11 14:25:56 -0600

    select random noises to play when phone is shaken

--- Sound_Shaker/ViewController.swift
+++ Sound_Shaker/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    let noises = ["bottle", "camera", "click", "glass", "ice", "laughingBird", "woodPecker"]
+
+    var player = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.motionShake {
+            
+            let randomNoise = noises[Int(arc4random_uniform(UInt32(noises.count)))]
+            let audioPath = Bundle.main.path(forResource: String(randomNoise), ofType: "mp3")
+            print(randomNoise)
+            print("Shake shake shake, shake shake shake, shake your booty, shake your bootay!")
+            
+            do {
+                
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+                player.play()
+                
+            } catch {
+                
+               // Process any errors
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
